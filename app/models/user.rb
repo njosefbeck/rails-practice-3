f4baac1ed62c9e592e5a92ec42215d05215f0e43
Nathan Beck <njosefbeck@gmail.com>
2020-05-21 20:53:37 -0500

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   # Though we save a password_digest to the db,
   # has_secure_password adds a virtual "password" record to the user
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string
   # could also write "def self.digest(string)"
